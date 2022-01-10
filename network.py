d825748213298e4c927f56392f12dbd71efeefae
PaoloRusso137 <95961400+PaoloRusso137@users.noreply.github.com>
2022-01-10 16:17:20 +0100

    Update network.py

--- network.py
+++ network.py
@@ -25,7 +25,7 @@ class GeoLocalizationNet(nn.Module):
 
 
 
-def gem(x, p=2, eps=5e-6):
+def gem(x, p=1, eps=1e-7):
     return F.avg_pool2d(x.clamp(min=eps).pow(p), (x.size(-2), x.size(-1))).pow(1./p)
 
 class GeM(nn.Module):
